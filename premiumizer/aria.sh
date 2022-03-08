7ce84c3e04cb117c97afe0b849e5243b229059f5
mohitjoshi155 <58655292+mohitjoshi155@users.noreply.github.com>
2022-03-08 20:37:12 +0530

    Update aria.sh

--- premiumizer/aria.sh
+++ premiumizer/aria.sh
@@ -1,1 +1,2 @@
-
+#!/bin/bash
+aria2c --on-download-complete=/conf/rcloneup.sh --disable-ipv6=true --enable-rpc --rpc-allow-origin-all --rpc-listen-all --rpc-listen-port=6800 --rpc-secret=premiumizer --max-connection-per-server=16 --file-allocation=none --disk-cache=0
